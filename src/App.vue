a05008eb0c12f3c31b5b7c7ad4220623528c0d5a
Sol <solglz11@gmail.com>
2025-03-31 15:47:58 -0600

    Setup App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="job-header">
+    <router-link to="/jobs" class="job-header-title white--text">Job Listings</router-link>
+  </header>
+    <router-view/>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+.job-header-title {
+  font-size: 24px;
+  padding: 20px;
+  font-weight: 600;  
+  margin-top: 20px;
+}
+</style>
