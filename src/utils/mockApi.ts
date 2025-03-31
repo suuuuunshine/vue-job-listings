13e8dc11dcebf908ec1fce64e840f39535522302
Sol <solglz11@gmail.com>
2025-03-31 17:47:05 -0600

    Add mock api with filter by category and search query

--- src/utils/mockApi.ts
+++ src/utils/mockApi.ts
@@ -1,12 +1,38 @@
 import type { Job } from "../types/job";
 
+interface FetchJobsResponse {
+  jobs: Job[];
+  total: number;
+}
 
-export const fetchJobs = async (page: number, limit: number): Promise<Job[]> => {
-  const response = await fetch('/jobs.json'); 
-  const jobData: Job[] = await response.json(); 
-  
+export const fetchJobs = async (
+  page: number,
+  limit: number,
+  category: string,
+  searchQuery: string
+): Promise<FetchJobsResponse> => {
+  const response = await fetch("/jobs.json");
+  let jobData: Job[] = await response.json();
+
+  if (category) {
+    jobData = jobData.filter((job) => job.category === category);
+  }
+
+  if (searchQuery) {
+    jobData = jobData.filter(
+      (job) =>
+        job.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.location.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.company.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
+
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
-  
-  return jobData.slice(startIndex, endIndex); 
+
+  return {
+    jobs: jobData.slice(startIndex, endIndex),
+    total: jobData.length,
+  };
 };
