87f3abcfc2b9f0a5d32ed65af29b801a5f330b00
Sol <solglz11@gmail.com>
2025-03-31 15:50:06 -0600

    Fix header styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="job-header">
+  <header class="header">
     <router-link to="/jobs" class="job-header-title white--text">Job Listings</router-link>
   </header>
     <router-view/>
@@ -15,5 +15,14 @@ import { RouterLink } from 'vue-router';
   padding: 20px;
   font-weight: 600;  
   margin-top: 20px;
+}
+
+.header{
+  background-color: var(--magenta-haze);
+  color: white;
+  padding: 16px;
+  text-align: center;
+  border-radius: 8px;
+  margin: 20px;
 }
 </style>
