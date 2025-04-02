d838fd003629d093fed7c08a6175ade3207e9584
Sol <solglz11@gmail.com>
2025-04-01 20:19:11 -0600

    Fix routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import JobDetail from '../views/JobDetail.vue';
-import JobList from '../views/JobList.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import JobDetail from "../views/JobDetail.vue";
+import JobList from "../views/JobList.vue";
 
 const routes = [
   {
-    path: '/', 
-    redirect: '/jobs', 
+    path: "/",
+    redirect: "/jobs",
   },
   {
-    path: '/jobs', 
-    name: 'JobList',
+    path: "/jobs",
+    name: "job-list",
     component: JobList,
   },
   {
-    path: '/jobs/:id',
-    component: JobDetail
+    path: "/jobs/:id",
+    name: "job-description",
+    component: JobDetail,
   },
 ];
 
