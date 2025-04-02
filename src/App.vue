57df17624a9941ac8394403f67fb2433e6e1667c
Sol <solglz11@gmail.com>
2025-04-01 20:17:33 -0600

    Add application modal to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <header class="header">
-    <router-link to="/jobs" class="job-header-title white--text">Job Listings</router-link>
+    <router-link to="/jobs" class="job-header-title white--text"
+      >Job Listings</router-link
+    >
   </header>
-    <router-view/>
+  <router-view />
+  <JobApplicationModal />
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
+import JobApplicationModal from "../src/components/JobApplicationModal.vue";
 </script>
 
 <style lang="scss" scoped>
 .job-header-title {
   font-size: 24px;
   padding: 20px;
-  font-weight: 600;  
+  font-weight: 600;
   margin-top: 20px;
 }
 
-.header{
-  background-color: var(--magenta-haze);
+.header {
+  background-color: var(--dark-blue);
   color: white;
   padding: 16px;
   text-align: center;
