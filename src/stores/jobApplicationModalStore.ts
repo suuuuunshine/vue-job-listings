c27a7918a2339dcd71ea23315bf789af9da8f0ee
Sol <solglz11@gmail.com>
2025-04-01 20:19:35 -0600

    Create store for modal

---
+++ src/stores/jobApplicationModalStore.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useModalStore = defineStore("modal", () => {
+  const isVisible = ref(false);
+  const job = ref<{ title?: string; description?: string } | null>(null);
+
+  const openModal = (jobData: { title?: string; description?: string }) => {
+    job.value = jobData;
+    isVisible.value = true;
+  };
+
+  const closeModal = () => {
+    isVisible.value = false;
+    job.value = null;
+  };
+
+  return { isVisible, job, openModal, closeModal };
+});
