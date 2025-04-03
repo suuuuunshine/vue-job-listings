85578824d094d64b12597fffd5417b41397fc2fd
Sol <solglz11@gmail.com>
2025-04-02 20:49:19 -0600

    build project

--- src/stores/jobsStore.d.ts
+++ src/stores/jobsStore.d.ts
@@ -1,107 +1,80 @@
 import type { Job } from "../types/job";
-export declare const useJobsStore: import("pinia").StoreDefinition<
-  "jobsStore",
-  Pick<
-    {
-      jobs: import("vue").Ref<
-        {
-          id: number;
-          title: string;
-          category: string;
-          company: string;
-          description: string;
-          location: string;
-        }[],
-        | Job[]
-        | {
-            id: number;
-            title: string;
-            category: string;
-            company: string;
-            description: string;
-            location: string;
-          }[]
-      >;
-      page: import("vue").Ref<number, number>;
-      limit: import("vue").Ref<number, number>;
-      totalCount: import("vue").Ref<number, number>;
-      searchQuery: import("vue").Ref<string, string>;
-      fetchJobsForPage: () => Promise<void>;
-      nextPage: () => void;
-      previousPage: () => void;
-      filterJobs: (category: string) => void;
-      searchJobs: (query: string) => void;
-    },
-    "jobs" | "page" | "limit" | "totalCount" | "searchQuery"
-  >,
-  Pick<
-    {
-      jobs: import("vue").Ref<
-        {
-          id: number;
-          title: string;
-          category: string;
-          company: string;
-          description: string;
-          location: string;
-        }[],
-        | Job[]
-        | {
-            id: number;
-            title: string;
-            category: string;
-            company: string;
-            description: string;
-            location: string;
-          }[]
-      >;
-      page: import("vue").Ref<number, number>;
-      limit: import("vue").Ref<number, number>;
-      totalCount: import("vue").Ref<number, number>;
-      searchQuery: import("vue").Ref<string, string>;
-      fetchJobsForPage: () => Promise<void>;
-      nextPage: () => void;
-      previousPage: () => void;
-      filterJobs: (category: string) => void;
-      searchJobs: (query: string) => void;
-    },
-    never
-  >,
-  Pick<
-    {
-      jobs: import("vue").Ref<
-        {
-          id: number;
-          title: string;
-          category: string;
-          company: string;
-          description: string;
-          location: string;
-        }[],
-        | Job[]
-        | {
-            id: number;
-            title: string;
-            category: string;
-            company: string;
-            description: string;
-            location: string;
-          }[]
-      >;
-      page: import("vue").Ref<number, number>;
-      limit: import("vue").Ref<number, number>;
-      totalCount: import("vue").Ref<number, number>;
-      searchQuery: import("vue").Ref<string, string>;
-      fetchJobsForPage: () => Promise<void>;
-      nextPage: () => void;
-      previousPage: () => void;
-      filterJobs: (category: string) => void;
-      searchJobs: (query: string) => void;
-    },
-    | "fetchJobsForPage"
-    | "nextPage"
-    | "previousPage"
-    | "filterJobs"
-    | "searchJobs"
-  >
->;
+export declare const useJobsStore: import("pinia").StoreDefinition<"jobsStore", Pick<{
+    jobs: import("vue").Ref<{
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[], Job[] | {
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[]>;
+    page: import("vue").Ref<number, number>;
+    limit: import("vue").Ref<number, number>;
+    totalCount: import("vue").Ref<number, number>;
+    searchQuery: import("vue").Ref<string, string>;
+    selectedCategory: import("vue").Ref<string, string>;
+    fetchJobsForPage: () => Promise<void>;
+    nextPage: () => void;
+    previousPage: () => void;
+    filterJobs: (category: string) => void;
+    searchJobs: (query: string) => void;
+}, "jobs" | "page" | "limit" | "totalCount" | "searchQuery" | "selectedCategory">, Pick<{
+    jobs: import("vue").Ref<{
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[], Job[] | {
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[]>;
+    page: import("vue").Ref<number, number>;
+    limit: import("vue").Ref<number, number>;
+    totalCount: import("vue").Ref<number, number>;
+    searchQuery: import("vue").Ref<string, string>;
+    selectedCategory: import("vue").Ref<string, string>;
+    fetchJobsForPage: () => Promise<void>;
+    nextPage: () => void;
+    previousPage: () => void;
+    filterJobs: (category: string) => void;
+    searchJobs: (query: string) => void;
+}, never>, Pick<{
+    jobs: import("vue").Ref<{
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[], Job[] | {
+        id: number;
+        title: string;
+        category: string;
+        company: string;
+        description: string;
+        location: string;
+    }[]>;
+    page: import("vue").Ref<number, number>;
+    limit: import("vue").Ref<number, number>;
+    totalCount: import("vue").Ref<number, number>;
+    searchQuery: import("vue").Ref<string, string>;
+    selectedCategory: import("vue").Ref<string, string>;
+    fetchJobsForPage: () => Promise<void>;
+    nextPage: () => void;
+    previousPage: () => void;
+    filterJobs: (category: string) => void;
+    searchJobs: (query: string) => void;
+}, "fetchJobsForPage" | "nextPage" | "previousPage" | "filterJobs" | "searchJobs">>;
